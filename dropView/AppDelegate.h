95b978652fa181b4c5b5eceaf0fda28add9f21be
xcmy <liuyang@jingdizhiwa.cn>
2015-12-17 13:44:17 +0800

    Initial Commit

---
+++ dropView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dropView
+//
+//  Created by 刘洋 on 15/12/17.
+//  Copyright © 2015年 刘洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dropView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  dropView
+//
+//  Created by 刘洋 on 15/12/17.
+//  Copyright © 2015年 刘洋. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
